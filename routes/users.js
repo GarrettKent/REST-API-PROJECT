eb0496d3023101ca4d253f7562d4754b75bcb373
GarrettKent <46579862+GarrettKent@users.noreply.github.com>
2019-05-30 14:30:46 -0500

    validate email

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {User} = require("../models");
+const {User} = require('../models');
 const authorize = require('./authorize');
-const bcryptjs = require("bcryptjs");
+const bcryptjs = require('bcryptjs');
 
 //Using GET, a user that is authorized is retrieved with a status of 200. password, createdAt, and updatedAt are filtered out
-router.get("/", authorize , (req, res) => {
+router.get('/', authorize , (req, res) => {
     res.json({
       id: req.currentUser.id,
       firstName: req.currentUser.emailAddress,
@@ -16,7 +16,7 @@ router.get("/", authorize , (req, res) => {
 });
 
 //Using POST, a user is created
-router.post("/", (req, res, next) => { 
+router.post('/', (req, res, next) => { 
     if (req.body.emailAddress) {
         User.findOne({ where: { emailAddress: req.body.emailAddress }})
     .then( email => {            
@@ -34,7 +34,7 @@ router.post("/", (req, res, next) => {
                 res.status(201).end();
             })
             .catch (err => {
-                if (err.name === "SequelizeValidationError") {
+                if (err.name === 'SequelizeValidationError') {
                     err.message = "All data must be entered";
                     err.status = 400;
                     next(err);
